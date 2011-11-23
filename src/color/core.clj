4e071247a4759be17cf9abdb408a1fb84b198e70
Tim McCormack <cortex@brainonfire.net>
2011-11-23 17:19:44 -0500

    Change spread to generate in [0,1) instead of [0,2^n).

--- src/color/core.clj
+++ src/color/core.clj
@@ -2,11 +2,15 @@
 
 
 (defn spread [n]
-  "For an exponent n, produce a permutation of [0, 2^n - 1] such that
-successive values are approximately maximally separate."
-  (if (= n 1)
-    [0 1]
-    (let [co1 (spread (dec n))
-          h1 (map #(* 2 %) co1)
-          h2 (map inc h1)]
-      (concat h1 h2))))
+  "For an exponent n, produce a permutation of numbers from the cyclic range
+ [0, 1) such that successive values are approximately maximally separate.
+ Specifically, this produces a concatenated sequence of groups [0], [1/2],
+ [1/4, 3/4], [1/8, 3/8, 5/8, 7/8], etc. for a total collection of 2^n numbers."
+  (case n
+        0 []
+        1 [0 0.5]
+        (let [h1 (spread (dec n))
+              shift (Math/pow 2 (- n))
+              h2 (map (partial + shift) h1)]
+          (concat h1 h2))))
+
--- test/color/test/core.clj
+++ test/color/test/core.clj
@@ -2,5 +2,34 @@
   (:use [color.core])
   (:use [clojure.test]))
 
+;;;; Tets utils
+
+(defn partition-doubling
+  "Partition a collection into groups of 1, 2, 4, 8... etc."
+  ([coll]
+     (partition-doubling coll 1))
+  ([coll group1]
+     (cond
+      (empty? coll) []
+      (= (count coll) 1) [coll]
+      :else (cons (take group1 coll)
+                  (partition-doubling (drop group1 coll)
+                                      (* 2 group1))))))
+
+(deftest partdouble
+  (is (= (partition-doubling []) []))
+  (is (= (partition-doubling [1]) [[1]]))
+  (is (= (map set (partition-doubling (range 1 16)))
+         [#{1} #{2 3} #{4 5 6 7} #{8 9 10 11 12 13 14 15}])))
+
+;;;; Tests
+
 (deftest utils
-  (is [0 1] (spread 1)))
+  (is (= (spread 0)
+         []))
+  (is (= (map (comp int #(* % 2)) (spread 1))
+         [0 1]))
+  (let [spread4int (map (comp int #(* % 16)) (spread 4))]
+    (is (= (first spread4int) 0))
+    (is (= (map set (partition-doubling (rest spread4int)))
+           [#{8} #{4 12} #{2 6 10 14} #{1 3 5 7 9 11 13 15}]))))
